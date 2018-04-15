72ec197f980d8dc5123fdb018841854e675e5ff3
Chau <ctch5@mail.umsl.edu>
2018-04-15 10:44:41 -0500

    test

--- src/crop/models/crop.model.ts
+++ src/crop/models/crop.model.ts
@@ -11,6 +11,7 @@ export class CropVm extends SharedModelVm {
   @ApiModelPropertyOptional()
   name?: string;
   @ApiModelPropertyOptional({
+    type: String,
     isArray: true,
   })
   variety?: string[];
@@ -22,6 +23,7 @@ export class CropParams {
   @ApiModelPropertyOptional()
   name?: string;
   @ApiModelPropertyOptional({
+    type: String,
     isArray: true,
   })
   variety?: string[];
